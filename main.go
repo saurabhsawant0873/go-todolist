e08dbe6d070d5d9da37a94ef2555679827de99e0
Saurabh Sawant <saurabhsawant@Saurabhs-MacBook-Pro.local>
2022-06-24 10:08:04 -0400

    start rest server and add routes using chi router

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -44,10 +48,45 @@ func init() {
 	render = renderer.New()
 	session, err := mgo.Dial(hostname)
 	checkError(err)
+	// Three modes: Strong -> More stability withread and write operations
+	// Monotonic -> Somewhere between Strong and Eventual
+	// Eventual -> Focusses more on loadbalancing
 	session.SetMode(mgo.Monotonic, true)
 	session.DB(dbname)
 }
 
+func todolistHandler() http.Handler {
+
+	groupRouter := chi.NewRouter()
+	groupRouter.Group(func(r chi.Router) {
+		groupRouter.Get("/", getTodoList)
+		groupRouter.Post("/", createTodoList)
+		groupRouter.Put("/{id}", updatetodoList)
+		groupRouter.Delete("/{id}", deleteTodoList)
+	})
+	return groupRouter
+}
+
 func main() {
 
+	homeRouter := chi.NewRouter()
+	homeRouter.Use(middleware.Logger)
+
+	homeRouter.Get("/", homeHandler)
+	homeRouter.Mount("/todolist", todolistHandler)
+
+	server := &http.Server{
+		Addr:         port,
+		Handler:      homeRouter,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		log.Printf("Listen on port : %d", port)
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("Listen : %s \n", err)
+		}
+	}()
 }
